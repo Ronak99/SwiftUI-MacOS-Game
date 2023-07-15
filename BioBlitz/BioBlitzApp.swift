4f9ad711f50d6f3b971551932d9ec2dc2f2ab428
ronakpustack <ronak@pustack.com>
2023-07-15 19:28:07 +0530

    Initial Commit

---
+++ BioBlitz/BioBlitzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioBlitzApp.swift
+//  BioBlitz
+//
+//  Created by Ronak Pustack on 15/07/23.
+//
+
+import SwiftUI
+
+@main
+struct BioBlitzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
